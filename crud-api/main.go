03b4823ca3ad3ca12744c174ffc59183945037dd
diegolopezq95 <diegoarmlopezq@gmail.com>
2021-07-20 23:47:01 -0500

    Go get

--- crud-api/main.go
+++ crud-api/main.go
@@ -5,10 +5,13 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
+		// H is a shortcut for map[string]interface{}
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
 	})
 	err := r.Run()
 	if err != nil {
-		return 
+		return
 	}
 }
